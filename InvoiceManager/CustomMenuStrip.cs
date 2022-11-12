76ca3fddfbfffcd220777f0eff486aa94c2a5402
Xav <lilchrondog@hotmail.com>
2022-11-12 16:51:40 -0600

    Add project files.

---
+++ InvoiceManager/CustomMenuStrip.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace InvoiceManager
+{
+	class SideMenu : Panel
+	{
+		private List<Tab> m_Tabs = new List<Tab>();
+
+		public SideMenu()
+		{
+			BackColor = SystemColors.ControlDark;
+			Resize += SideMenu_Resize;
+		}
+
+		private void SideMenu_Resize(object sender, EventArgs e)
+		{
+			foreach(Tab t in m_Tabs)
+			{
+				t.Width = Width;
+			}
+		}
+
+		public void AddTab(string name)
+		{
+			Tab t = new Tab(this, name);
+			t.Location = new Point(0, m_Tabs.Count * Tab.TabHeight);
+			m_Tabs.Add(t);
+		}
+	}
+
+	class Tab : Label
+	{
+		public const int TabHeight = 48;
+		public const int FontSize = 14;
+		public const string FontFamily = "Segoe UI";
+
+		public Tab(Control parent, string name)
+		{
+			parent.Controls.Add(this);
+			TextAlign = ContentAlignment.MiddleRight;
+			Text = name;
+			Height = TabHeight;
+			Width = Parent.Width;
+			MouseMove += Tab_MouseMove;
+			MouseLeave += Tab_MouseLeave;
+			MouseEnter += Tab_MouseEnter;
+			Font = new Font(FontFamily, FontSize);
+		}
+
+		private void Tab_MouseEnter(object sender, EventArgs e)
+		{
+			ForeColor = SystemColors.ControlLight;
+		}
+
+		private void Tab_MouseLeave(object sender, EventArgs e)
+		{
+			ForeColor = SystemColors.ControlText;
+		}
+
+		Rectangle ImageArea(Control c)
+		{
+			Size si = c.Size;
+			Size sp = c.ClientSize;
+			float ri = 1f * si.Width / si.Height;
+			float rp = 1f * sp.Width / sp.Height;
+			if (rp > ri)
+			{
+				int width = si.Width * sp.Height / si.Height;
+				int left = (sp.Width - width) / 2;
+				return new Rectangle(left, 0, width, sp.Height);
+			}
+			else
+			{
+				int height = si.Height * sp.Width / si.Width;
+				int top = (sp.Height - height) / 2;
+				return new Rectangle(0, top, sp.Width, height);
+			}
+		}
+
+		private void Tab_MouseMove(object sender, MouseEventArgs e)
+		{
+			Cursor = ImageArea(this).Contains(e.Location) ? Cursors.Hand : Cursors.Default;
+			//ForeColor = ImageArea(this).Contains(e.Location) ? SystemColors.HighlightText : SystemColors.GrayText;
+		}
+	}
+}
---
+++ InvoiceManager/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace InvoiceManager
+{
+	public partial class Form1 : Form
+	{
+		SideMenu m_Menu = new SideMenu();
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			m_Menu.Width = (Width / 5) > 256 ? 256 : Width / 5;
+			m_Menu.Height = Height;
+			m_Menu.Location = new Point(0, 0);
+			m_Menu.AddTab("Expenses");
+			m_Menu.AddTab("Invoicing");
+			m_Menu.AddTab("Budgeting");
+			Controls.Add(m_Menu);
+
+			Resize += Form1_Resize;
+		}
+
+		private void Form1_Resize(object sender, EventArgs e)
+		{
+			m_Menu.Width = (Width / 5) > 256 ? 256 : Width / 5;
+			m_Menu.Height = Height;
+		}
+	}
+}
